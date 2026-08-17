d946d8c11bffc8e6a71fa21b009923a2eae07844
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-17 14:20:08 +0700

    upload: lib/screens/search_screen.dart

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,4 @@
+// Pencarian terintegrasi langsung di HomeScreen (search bar + hasil dalam
+// satu layar, mengikuti pola Spotify). File ini di-reexport agar struktur
+// folder tetap sesuai konvensi `screens/search`.
+export 'home_screen.dart' show HomeScreen;
